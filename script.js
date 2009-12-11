a853bb4214a27b8b1bb8f5493021ce266454f31d
Adrian Lang <lang@cosmocode.de>
2009-12-11 14:52:11 +0100

    Init.

---
+++ script.js
@@ -0,0 +1,43 @@
+var ajax_loader = {
+
+    sack_form: function (form, func) {
+        var ajax = new sack(DOKU_BASE + 'lib/exe/ajax.php');
+        function serializeByTag(tag) {
+            var inps = form.getElementsByTagName(tag);
+            for (var inp in inps) {
+                if (inps[inp].name) {
+                    ajax.setVar(inps[inp].name, inps[inp].value);
+                }
+            }
+        }
+        serializeByTag('input');
+        serializeByTag('textarea');
+        ajax.elementObj = form.parentNode;
+        ajax.afterCompletion = func;
+        ajax.runAJAX();
+        return false;
+    },
+
+    start: function () {
+        var forms = getElementsByClass('ajax_loader', document, 'form');
+        if (forms.length > 0) sack_form(forms[0], ajax_loader.start);
+    },
+
+    Loader: function (plugin, data) {
+        var form = document.createElement('form');
+        form.className = 'ajax_loader';
+        form.addHidden = function (name, value) {
+            this.innerHTML += '<input type="hidden" name="' + name + '" value="' + value +
+                              '" />';
+        };
+        form.addHidden('call', 'ajax_loader_' + plugin);
+        for (var k in data) {
+            form.addHidden('ajax_loader_data[' + k + ']', data[k]);
+        }
+        return form;
+    }
+
+
+};
+
+addInitEvent(ajax_loader.start);
